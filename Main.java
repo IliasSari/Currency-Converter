447222beb073cb4ea0d35bcc1a278b9f46969ba2
IliasSari <sarigilias@gmail.com>
2026-03-12 11:44:42 +0200

    Added some currencies more

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main {
         Currency usd = new Currency("USA Dollar", "$", 1.09);
         Currency gbp = new Currency("British Pound", "£", 0.85);
         Currency jpy = new Currency("Japanese Yen", "¥", 162.50);
+        Currency aud = new Currency("Australian Dollar", "AUD $", 1.62);
+        Currency inr = new Currency("Indian Rupee", "₹", 106.51);
+        Currency cad = new Currency("Canadian Dollar", "CAD $", 1.57);
 
         System.out.print("Enter the amount of euro (€): ");
         double amount = input.nextDouble();
@@ -17,6 +20,9 @@ public class Main {
         usd.convertAndPrint(amount);
         gbp.convertAndPrint(amount);
         jpy.convertAndPrint(amount);
+        aud.convertAndPrint(amount);
+        inr.convertAndPrint(amount);
+        cad.convertAndPrint(amount);
         input.close();
     }
 }
